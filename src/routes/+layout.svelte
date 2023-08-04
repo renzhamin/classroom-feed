33f3959cc6f4600967ae6b84c0bacbd2673f4ba3
renzhamin <renzhamin@gmail.com>
2023-08-05 01:25:03 +0600

    remove auto update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,6 @@
 <script>
     import "../app.postcss";
-    import { onMount } from "svelte";
     import { pwaInfo } from "virtual:pwa-info";
-
-    onMount(async () => {
-        if (pwaInfo) {
-            const { registerSW } = await import("virtual:pwa-register");
-            registerSW({
-                immediate: true,
-                onRegistered(r) {
-                    // uncomment following code if you want check for updates
-                    // r && setInterval(() => {
-                    //    console.log('Checking for sw update')
-                    //    r.update()
-                    // }, 20000 /* 20s for testing purposes */)
-                    console.log(`SW Registered: ${r}`);
-                },
-                onRegisterError(error) {
-                    console.log("SW registration error", error);
-                },
-            });
-        }
-    });
 
     $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : "";
 </script>
@@ -33,7 +12,3 @@
 <main>
     <slot />
 </main>
-
-{#await import("$lib/ReloadPrompt.svelte") then { default: ReloadPrompt }}
-    <ReloadPrompt />
-{/await}
